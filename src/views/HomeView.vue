23b8c183c69f1b42d5c5134f838fa17240077471
Claudia Miranda Ch <externo340@compartamos.pe>
2023-05-02 22:30:00 -0500

    + image similar to landing & fix style in welcome

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,34 @@
 <template>
   <RouterView />
   <div class="home">
-    <h1 class="mr-2">Mecanillama</h1>
-    <p>The app for connecting driver and mechanic</p>
-    <div class="buttons">
+    <h1 class="mr-2" style="
+    font-family: fantasy;
+    font-size: xxx-large;
+    ">Mecanillama</h1>
+    <h2>The app for connecting driver and mechanic</h2>
+        <div class="mb-3">
+      <img
+        style="
+           {
+            height: 228px;
+            width: 400px;
+          }
+        "
+        src="src/images/hero-img.jfif"
+        alt="man over a car"
+      />
+    </div>
+    <div class="buttons" >
       <div class="button">
         <RouterLink to="/sign-up">
-          <pv-button label="Sign up" class="p-button-rounded p-button-lg" />
+          <pv-button label="Sign up" class="p-button-rounded p-button-lg" style="padding: 0.8925rem 8.25rem;"/>
         </RouterLink>
       </div>
       <div class="button">
         <RouterLink to="/sign-in">
-          <pv-button label="Sign in" class="p-button-rounded p-button-lg" />
+          <pv-button label="Sign in" class="p-button-rounded p-button-lg" style="padding: 0.8925rem 8.25rem;"/>
         </RouterLink>
       </div>
-    </div>
-    <div class="mb-3">
-      <img
-        style="
-           {
-            height: 360px;
-            width: 640px;
-          }
-        "
-        src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg"
-        alt="man over a car"
-      />
     </div>
   </div>
 </template>
